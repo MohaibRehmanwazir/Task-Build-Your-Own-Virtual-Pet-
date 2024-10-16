bfb5dcf2b87a46d83bfd222a105cbf2649b8f2d8
Mohaib Rehman <mohibrahman973@gmail.com>
2024-10-16 10:17:37 +0500

    Add the function

--- Scripet.js
+++ Scripet.js
@@ -8,33 +8,33 @@ var pet = {
     
     //Add Methods 
 
-//     feed: function(){
-//         this.hunger -= 20; //Decrease hunger by 20
-//         if(this.hunger< 0){
-//            this.hunger = 0; // hunger doesn't go below 0
+    feed: function(){
+        this.hunger -= 20; //Decrease hunger by 20
+        if(this.hunger< 0){
+           this.hunger = 0; // hunger doesn't go below 0
 
-//         }
-//         alert(`${this.name} has been fed! the hunger level is now ${this.hunger}.`);
-//    },
-//    play: function(){
-//        this.happiness +=20;
-//        if(this.happiness > 100){
-//            this.happiness = 100; // happiness doesn't exceed 100
-//        }
-//        alert(`${this.name} played! happiness level is now ${this.happiness}.`);
-//    },
-//    agePet: function(){
-//        this.age +=1;
-//        this.happiness -= 5;
-//        this.hunger +=10;
-//        if(this.happiness > 0){
-//            this.happiness = 0;
-//        }
-//        if(this.hunger <100){
-//            this.hunger = 100;
-//        }
-//        alert(`${this.name} has aged! Age:${this.age}, happiness:${this.happiness}, Hunger:${this.hunger}`)
-//    }
+        }
+        alert(`${this.name} has been fed! the hunger level is now ${this.hunger}.`);
+   },
+   play: function(){
+       this.happiness +=20;
+       if(this.happiness > 100){
+           this.happiness = 100; // 
+       }
+       alert(`${this.name} played! happiness level is now ${this.happiness}.`);
+   },
+   agePet: function(){
+       this.age +=1;
+       this.happiness -= 5;
+       this.hunger +=10;
+       if(this.happiness > 0){
+           this.happiness = 0;
+       }
+       if(this.hunger <100){
+           this.hunger = 100;
+       }
+       alert(`${this.name} has aged! Age:${this.age}, happiness:${this.happiness}, Hunger:${this.hunger}`)
+   }
 
 
 
