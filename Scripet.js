247d8f0955ec20d3482f512b54ba63e5e1d0557b
Mohaib Rehman <mohibrahman973@gmail.com>
2024-10-16 10:08:45 +0500

    Add the index file

---
+++ Scripet.js
@@ -0,0 +1,16 @@
+var petName = prompt("What is your pet name?");
+alert(petName);
+
+var petkind = prompt("what kind of pets is it (dog, cat, dragon) ");
+alert(petkind);
+
+var old = prompt("HOw Old is your pet");
+alert(old);
+
+var happy = prompt("how much is Happy  your pet");
+aler(happy);
+
+var haugry = prompt("how hungery your pet");
+alert(haugry);
+
+var userinput = prompt("What would you like to do with size \n-feed\n2-play\n3-age\n2-quit")
