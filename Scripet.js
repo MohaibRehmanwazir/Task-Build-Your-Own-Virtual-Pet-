fd213898809dfeb856f3e38e815e51ee74ccd0a8
Mohaib Rehman <mohibrahman973@gmail.com>
2024-10-16 10:12:26 +0500

    Create an object

--- Scripet.js
+++ Scripet.js
@@ -1,16 +1,69 @@
-var petName = prompt("What is your pet name?");
-alert(petName);
+//Create a pet Object 
+var pet = {
+    name:"Poppy",
+    type: "dog",
+    age: 3,
+    happiness:30,
+    hunger:45,
+    
+    //Add Methods 
 
-var petkind = prompt("what kind of pets is it (dog, cat, dragon) ");
-alert(petkind);
+//     feed: function(){
+//         this.hunger -= 20; //Decrease hunger by 20
+//         if(this.hunger< 0){
+//            this.hunger = 0; // hunger doesn't go below 0
 
-var old = prompt("HOw Old is your pet");
-alert(old);
+//         }
+//         alert(`${this.name} has been fed! the hunger level is now ${this.hunger}.`);
+//    },
+//    play: function(){
+//        this.happiness +=20;
+//        if(this.happiness > 100){
+//            this.happiness = 100; // happiness doesn't exceed 100
+//        }
+//        alert(`${this.name} played! happiness level is now ${this.happiness}.`);
+//    },
+//    agePet: function(){
+//        this.age +=1;
+//        this.happiness -= 5;
+//        this.hunger +=10;
+//        if(this.happiness > 0){
+//            this.happiness = 0;
+//        }
+//        if(this.hunger <100){
+//            this.hunger = 100;
+//        }
+//        alert(`${this.name} has aged! Age:${this.age}, happiness:${this.happiness}, Hunger:${this.hunger}`)
+//    }
 
-var happy = prompt("how much is Happy  your pet");
-aler(happy);
 
-var haugry = prompt("how hungery your pet");
-alert(haugry);
 
-var userinput = prompt("What would you like to do with size \n-feed\n2-play\n3-age\n2-quit")
+// }
+
+// function interactive(){
+
+    
+//     while(true){
+//     //Ask from user what they want to do
+
+//     let action = prompt("What would you like to do? (feed,age,play,etc...");
+//     if(action === "feed"){
+//         pet.feed();
+        
+//     }else if(action === "play"){
+//         pet.play();
+//     }else if(action === "age"){
+//         pet.agePet();
+        
+//     }else if(action === "exit"){
+//         alert("thanks for taking care of your pet!");
+//         break;
+//     }else{
+//         alert("Invalid action.Please choose another action")
+//     }
+// }
+
+// }
+
+
+// interactive()
